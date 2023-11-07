3fa1b18e7097e35a90b5b906913afe2a51296e0f
Timur <timur87@mail.ru>
2023-11-07 18:10:18 +0300

    first commit

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -4,15 +4,26 @@ function mobileNav() {
   const navBtnClose = document.querySelector("#modalClose");
   const navOverlay = document.querySelector("#mobileNavOverlay");
   const nav = document.querySelector("#mobileNav");
+  const navLink = document.querySelectorAll(".mobile-nav__list a");
 
   navBtnOpen.onclick = toggleMobileNav;
   navBtnClose.onclick = toggleMobileNav;
   navOverlay.onclick = toggleMobileNav;
 
+  navLink.forEach((link) => {
+    link.onclick = removeMobileNav;
+  });
+
   function toggleMobileNav() {
     navOverlay.classList.toggle("mobile-nav-overlay--open");
     nav.classList.toggle("mobile-nav--open");
     document.body.classList.toggle("no-scroll");
+  }
+
+  function removeMobileNav() {
+    navOverlay.classList.remove("mobile-nav-overlay--open");
+    nav.classList.remove("mobile-nav--open");
+    document.body.classList.remove("no-scroll");
   }
 }
 
